7635bb72377845823f10234f2a09dc1b713adca1
pawelszpunar <78647588+pawelszpunar@users.noreply.github.com>
2021-05-27 21:35:37 +0200

    localization class started

--- src/main/java/Localization.java
+++ src/main/java/Localization.java
@@ -1,4 +1,26 @@
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity(name = "localization")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Localization {
+
+    @Id
+    @GeneratedValue
+    private Integer id;
+
+
+
+
+
 
 
 }
