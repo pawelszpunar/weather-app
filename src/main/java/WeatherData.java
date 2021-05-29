7bfa9968f40f8d88e70bde791f5b85ea1c632133
pawelszpunar <78647588+pawelszpunar@users.noreply.github.com>
2021-05-29 09:03:04 +0200

    project preparation 3

--- src/main/java/WeatherData.java
+++ src/main/java/WeatherData.java
@@ -13,6 +13,15 @@ import javax.persistence.Id;
 @AllArgsConstructor
 public class WeatherData {
 
+    public WeatherData(Integer temperature, Integer pressure, Integer humidity, Integer windSpeed, Integer windDegree, Integer localization_id) {
+        this.temperature = temperature;
+        this.pressure = pressure;
+        this.humidity = humidity;
+        this.windSpeed = windSpeed;
+        this.windDegree = windDegree;
+        this.localization_id = localization_id;
+    }
+
     @Id
     @GeneratedValue
     private Integer id;
@@ -31,5 +40,8 @@ public class WeatherData {
 
     @Column(name = "wind_degree")
     private  Integer windDegree;
+
+    @Column(name = "localization_id")
+    private Integer localization_id;
 
 }
