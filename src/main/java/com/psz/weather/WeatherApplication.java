6da3d1da74033b1fb27c67eb512dec458dec46b5
pawelszpunar <78647588+pawelszpunar@users.noreply.github.com>
2021-05-29 23:33:28 +0200

    add new location completed and working!

---
+++ src/main/java/com/psz/weather/WeatherApplication.java
@@ -0,0 +1,11 @@
+package com.psz.weather;
+
+public class WeatherApplication {
+    public static void main(String[] args) {
+        LocationRepositoryImpl locationRepositoryImpl = new LocationRepositoryImpl();
+        LocationService locationService = new LocationService(locationRepositoryImpl);
+        LocationController locationController = new LocationController(locationService);
+        UserInterface userInterface = new UserInterface(locationController);
+        userInterface.run();
+    }
+}
